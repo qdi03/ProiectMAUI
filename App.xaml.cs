120343c1f9fbd3b8eb46b873669db5786c8d6fb3
ADRIAN MEZEI-RACZ <adrian.mezei@stud.ubbcluj.ro>
2025-01-12 16:34:56 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ProiectMAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
